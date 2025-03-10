182c947017648c832bbc31c826cdc608c3e8d113
Wen Zhen <wlzeign@hotmail.com>
2025-03-10 19:09:29 +0800

    Update index.tsx

--- src/components/VegaLite/index.tsx
+++ src/components/VegaLite/index.tsx
@@ -1,30 +1,11 @@
 'use client'
 
-import {compile, Config, TopLevelSpec} from 'vega-lite';
+import {compile, Config} from 'vega-lite';
 import vegaEmbed from 'vega-embed';
 import {useEffect, useMemo, useRef} from "react";
 
-const VegaLite = ({vegaString}: {vegaString?: string}) => {
+const VegaLite = ({vegaString, width, height}: { vegaString?: string, width?: number, height?: number }) => {
   const visRef = useRef<HTMLDivElement>(null);
-
-  const vegaLiteSpec: TopLevelSpec = {
-    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-    data: {url: "/vispilot/data/cars.csv"},
-    "mark":"line",
-    "encoding": {
-        "x": {
-            "field":"Year",
-            "type":"temporal",
-          },
-        "y": {
-            "field":"MPG",
-            "type":"quantitative",
-          }
-      },
-    width: 230,
-    height: 110,
-    padding: 0,
-  };
 
   const config: Config = {
     bar: {},
@@ -37,29 +18,24 @@ const VegaLite = ({vegaString}: {vegaString?: string}) => {
     try {
       const spec = JSON.parse(vegaString || '{}');
       spec.data = {url: '/vispilot/data/' + spec.data?.name || ''}
-      spec.width = 230;
-      spec.height = 110;
+      spec.width = width || 230;
+      spec.height = height || 110;
       spec.padding = 5;
       return compile(spec, {config}).spec;
     } catch (e) {
-      return compile(vegaLiteSpec, {config}).spec;
+      return null;
     }
-  }, [vegaString, vegaLiteSpec, config]);
+  }, [vegaString, width, height, config]);
 
   useEffect(() => {
-    vegaEmbed(visRef.current!, spec, {actions: false})
-      .then(function (result) {
-        // add class on the visref
-        if (visRef.current) {
-          // console.log('Render', spec)
-          // visRef.current.classList.add('hover:shadow-lg', '!cursor-pointer');
-        }
-      })
-      .catch(console.error);
+    if (spec) {
+      vegaEmbed(visRef.current!, spec, {actions: false})
+        .then()
+        .catch(console.error);
     }
-  )
+  })
   return (
-    <div ref={visRef}></div>
+    <div ref={visRef}/>
   );
 }
 
